6c11b6b19033be04ccd700d3307c11d70df7ea72
THU-wyw <wyw0714@gmail.com>
2014-12-16 19:16:46 +0800

    静态版本

--- urlhandler/userpage/static1/js/activity.js
+++ urlhandler/userpage/static1/js/activity.js
@@ -1,33 +1,27 @@
-var data = {
-	poster_url: "img/test.jpg",
-	description: "袁扬姐姐太强了！"
-}
-
-var tplHTML='<div id="home" class="ui-scroller">' + 
-            '<div id="home" style="margin-left:10%; margin-right:10%; padding-top:30px; text-align:center">' +
-            '<img id="poster" src="<%=poster_url%>"/>' +
-            '<p id="description"><%=description%>$(/</p>' + 
-           	'</div>';
-
-//var dest = $.tpl(tplHTML, data);
-
-
 window.addEventListener('load', function(){
-	//$('#home').html(dest);
-	/*var el=$.loading({
-		content: '加载中...'
-	})*/
-	//el.loading("hide");
-    //homepage.render();
-    //homepage.fetch();
-    //candidate_list.render();
-    //candidate_list.fetch();
-    model.fetch();
-    /*var tab = new Scroll('.ui-tab', {
-        role: 'tab',
-        autoplay: false,
-        interval: 3000
-    });*/
+    //model.fetch();
+        show_button = $('.show');
+        hide_button = $('.hide');
+        for (var i = 0; i < show_button.length; i++) {
+            $(show_button[i]).click((function (i){
+                return function (){
+                    $('.candidate-li:eq(' + i + ')').removeClass('over_hidden');
+                    $('.candidate_img_container:eq(' + i + ')').removeClass('content_top');
+                    $(show_button[i]).addClass('hidden');
+                    $(hide_button[i]).removeClass('hidden');
+                }
+            })(i))
+        }
+        for (var i = 0; i < hide_button.length; i++) {
+            $(hide_button[i]).click((function (i){
+                return function (){
+                    $('.candidate-li:eq(' + i + ')').addClass('over_hidden');
+                    $('.candidate_img_container:eq(' + i + ')').addClass('content_top');
+                    $(hide_button[i]).addClass('hidden');
+                    $(show_button[i]).removeClass('hidden');
+                }
+            })(i))
+        }
     var tab = new Navigation('.ui-tab-nav', '.ui-tab-content');
 })    
 
