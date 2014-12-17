dc528a7c4e4931964da86a74a93e38a17e2f124b
THU-wyw <wyw0714@gmail.com>
2014-12-17 19:15:30 +0800

    将activity.js文件合并至app.js中

--- urlhandler/userpage/static1/js/activity.js
+++ urlhandler/userpage/static1/js/activity.js
@@ -3,58 +3,6 @@ var debugView = false
 window.addEventListener('load', function(){
     if (!debugView) {
         model.fetch();
-
-        desc = $('.candidate_info');
-        console.log(desc);
-        show_button = $('.show');
-        hide_button = $('.hide');
-        real_checkbox = $('input');
-        fake_checkbox = $('.icon-unselected');
-        for (var i = 0; i < show_button.length; i++) {
-            console.log($(desc[i]).height());
-            if($(desc[i]).height() < 210){
-                console.log(i+"too short");
-                $(show_button[i]).remove();
-                continue;
-            }
-            $(show_button[i]).click((function (i){
-                return function (){
-                    $('.candidate-li:eq(' + i + ')').removeClass('over_hidden');
-                    $('.candidate_img_container:eq(' + i + ')').removeClass('content_top');
-                    $(show_button[i]).addClass('hidden');
-                    $(hide_button[i]).removeClass('hidden');
-                }
-            })(i))
-        }
-        for (var i = 0; i < hide_button.length; i++) {
-            if($(desc[i]).height() < 210){
-                $(hide_button[i]).remove();
-                continue;
-            }
-            $(hide_button[i]).click((function (i){
-                return function (){
-                    $('.candidate-li:eq(' + i + ')').addClass('over_hidden');
-                    $('.candidate_img_container:eq(' + i + ')').addClass('content_top');
-                    $(hide_button[i]).addClass('hidden');
-                    $(show_button[i]).removeClass('hidden');
-                }
-            })(i))
-        }
-        for (var i = 0; i < fake_checkbox.length; i++) {
-            $(fake_checkbox[i]).click((function (i){
-                return function (){
-                    if($(fake_checkbox[i]).attr("class") == "icon-unselected") {
-                        $(fake_checkbox[i]).addClass('icon-selected');
-                        $(fake_checkbox[i]).removeClass('icon-unselected');
-                        real_checkbox[i].checked = true;
-                    }else{
-                        $(fake_checkbox[i]).addClass('icon-unselected');
-                        $(fake_checkbox[i]).removeClass('icon-selected');
-                        real_checkbox[i].checked = false;
-                    }
-                }
-            })(i))
-        }
     }
     var tab = new Navigation('.ui-tab-nav', '.ui-tab-content');
 })    
